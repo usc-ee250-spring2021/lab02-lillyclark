37288da56870d1703a993424a7c81347422f66ac
lillyclark <lilliamc@usc.edu>
2021-02-01 15:47:59 -0800

    lab02 demo (no ultrasonic sensor)

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -23,16 +23,21 @@ sys.path.append('../../Software/Python/')
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
+from grove_rgb_lcd import *
 
 """This if-statement checks if you are running this python file directly. That 
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
 if __name__ == '__main__':
-    PORT = 4    # D4
-
+    rotary_sensor = 0    # D4
+    setRGB(0,255,0)
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
 
-        print(grovepi.ultrasonicRead(PORT))
+        threshold = grovepi.analogRead(rotary_sensor)
+        print(threshold)
+        setText_norefresh(str(threshold))
+        #print(grovepi.ultrasonicRead(PORT))
+
